5607f1d7ea0b20bfee620c69a659235fd3d3f9ac
Kanchan Bandesha <147580734+kanchanb03@users.noreply.github.com>
2025-12-11 20:54:00 -0600

    Update settings.py

--- myproject/project/settings.py
+++ myproject/project/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = ["flashflicks-production.up.railway.app",
     "localhost",
     "127.0.0.1",]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://flashflicks-production.up.railway.app",
+]
+
+
 
 # Application definition
 
